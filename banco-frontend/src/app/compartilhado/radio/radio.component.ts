e90581ef1556c2697f5b47e0fbc3b78746ff5392
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-09-04 23:34:32 -0300

    Frontend - Implementado o ControlValueAccessor

--- banco-frontend/src/app/compartilhado/radio/radio.component.ts
+++ banco-frontend/src/app/compartilhado/radio/radio.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'bc-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: Array<RadioOption>;
 
   valor: any;
+  onChange: any;
 
   constructor() { }
 
@@ -19,6 +28,21 @@ export class RadioComponent implements OnInit {
   setValor(valor: any) {
 
     this.valor = valor;
+    this.onChange(this.valor);
   }
+
+  writeValue(obj: any): void {
+    
+    this.valor = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void { }
+
+  setDisabledState?(isDisabled: boolean): void { }
 
 }
