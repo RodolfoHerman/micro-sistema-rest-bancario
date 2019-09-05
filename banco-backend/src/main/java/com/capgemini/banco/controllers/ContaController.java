9d1f0a0ec18d47a94a3e9cfd0eed2c8e8b185ccd
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-09-05 01:49:05 -0300

    Backend - Habilitando o cors origin de forma global

--- banco-backend/src/main/java/com/capgemini/banco/controllers/ContaController.java
+++ banco-backend/src/main/java/com/capgemini/banco/controllers/ContaController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/contas")
-@CrossOrigin(origins = "*")
 public class ContaController {
 
     @Autowired
